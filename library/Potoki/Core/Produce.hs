34816866a46524d3a1a3e1a5916536e79f39343e
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-11-29 11:44:59 +0300

    Fix Produce Pure

--- library/Potoki/Core/Produce.hs
+++ library/Potoki/Core/Produce.hs
@@ -16,7 +16,7 @@ deriving instance Functor Produce
 
 instance Applicative Produce where
   pure x =
-    Produce (pure (pure x, pure ()))
+    list [x]
   (<*>) (Produce leftIO) (Produce rightIO) =
     Produce $ do
       (leftFetch, leftKill) <- leftIO
