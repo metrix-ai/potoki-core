3ca621de49bd5f6ee619a279c1613fc26cd2941d
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-02-25 13:02:41 +0300

    Factor a piece of Choice out to Fetch

--- library/Potoki/Core/Transform.hs
+++ library/Potoki/Core/Transform.hs
@@ -28,7 +28,7 @@ instance Profunctor Transform where
 
 instance Choice Transform where
   right' (Transform rightTransformIO) =
-    Transform $ \ (A.Fetch eitherFetchIO) -> do
+    Transform $ \ eitherFetch -> do
       fetchedLeftMaybeRef <- newIORef Nothing
       rightFetchMaybeRef <- newIORef Nothing
       return $ A.Fetch $ \ nil just -> do
@@ -37,9 +37,7 @@ instance Choice Transform where
           case rightFetchMaybe of
             Just rightFetch -> return rightFetch
             Nothing -> do
-              rightFetch <- rightTransformIO $ A.Fetch $ \ nil just -> join $ eitherFetchIO (return nil) $ \ case
-                Right !rightInput -> return (just rightInput)
-                Left !leftInput -> writeIORef fetchedLeftMaybeRef (Just leftInput) $> nil
+              rightFetch <- rightTransformIO (A.rightCachingLeft fetchedLeftMaybeRef eitherFetch)
               writeIORef rightFetchMaybeRef (Just rightFetch)
               return rightFetch
         join $ rightFetchIO
