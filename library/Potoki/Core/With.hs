6bd776f5e2de9154ad2e07768752bd676e7b10a6
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-05-11 16:03:32 +0300

    Add finally to With

--- library/Potoki/Core/With.hs
+++ library/Potoki/Core/With.hs
@@ -22,7 +22,7 @@ instance Applicative With where
     With $ do
       (f, release1) <- io1
       (x, release2) <- onException io2 release1
-      return (f x, release2 >> release1)
+      return (f x, finally release2 release1)
 
 instance Monad With where
   return = pure
@@ -30,7 +30,7 @@ instance Monad With where
     With $ do
       (resource1, release1) <- io1
       (resource2, release2) <- case k2 resource1 of With io2 -> onException io2 release1
-      return (resource2, release2 >> release1)
+      return (resource2, finally release2 release1)
 
 instance MonadIO With where
   liftIO io =
