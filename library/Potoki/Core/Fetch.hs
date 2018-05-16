f0e8a797179c6c09014f8db46b27f9c5f1d73553
Pluralia <irina@metrix.ai>
2018-05-16 18:53:35 +0300

    fix Fetch.hs

--- library/Potoki/Core/Fetch.hs
+++ library/Potoki/Core/Fetch.hs
@@ -53,48 +53,46 @@ instance MonadPlus Fetch where
 duplicate :: Fetch element -> IO (Fetch element, Fetch element)
 duplicate (Fetch fetchIO) =
   do
-    leftBuffer  <- newTQueueIO
+    leftBuffer <- newTQueueIO
     rightBuffer <- newTQueueIO
     notFetchingVar <- newTVarIO True
-    notEndVar      <- newTVarIO True
+    notEndVar <- newTVarIO True
     let
       newFetch ownBuffer mirrorBuffer =
         Fetch $ do
           fetch <- fetchIO
-          join
-            (atomically
-               (mplus
-                  (do
-                     element <- readTQueue ownBuffer
-                     return $ return (Just element)
-                  )
-                  (do
-                     notEnd <- readTVar notEndVar
-                     if notEnd
+          join $
+            atomically
+             (mplus
+                (do
+                   element <- readTQueue ownBuffer
+                   return $ return (Just element)
+                )
+                (do
+                   notEnd <- readTVar notEndVar
+                   if notEnd
                      then do
                        notFetching <- readTVar notFetchingVar
                        guard notFetching
                        writeTVar notFetchingVar False
-                       return $ 
-                         case fetch of
-                           Nothing      -> do
-                             atomically
-                               (do
-                                  writeTVar notEndVar False
-                                  writeTVar notFetchingVar True
-                               )
-                             return Nothing
-                           Just !element -> do
-                             atomically
-                               (do
-                                  writeTQueue mirrorBuffer element
-                                  writeTVar notFetchingVar True
-                               )
-                             return (Just element)
+                       return $ case fetch of
+                         Nothing      -> do
+                           atomically
+                             (do
+                                writeTVar notEndVar False
+                                writeTVar notFetchingVar True
+                             )
+                           return Nothing
+                         Just !element -> do
+                           atomically
+                             (do
+                                writeTQueue mirrorBuffer element
+                                writeTVar notFetchingVar True
+                             )
+                           return $ Just element
                      else return $ return Nothing
-                  )
-               )
-            ) 
+                )
+             )
       leftFetch =
         newFetch leftBuffer rightBuffer
       rightFetch =
@@ -118,7 +116,7 @@ list unsentListRef =
   Fetch $ do
     refList <- readIORef unsentListRef
     case refList of
-      (!head) : tail -> do
+      (!head) : (!tail) -> do
         writeIORef unsentListRef tail
         return $ Just head
       _              -> do
@@ -128,34 +126,34 @@ list unsentListRef =
 {-# INLINABLE firstCachingSecond #-}
 firstCachingSecond :: IORef b -> Fetch (a, b) -> Fetch a
 firstCachingSecond cacheRef (Fetch bothFetchIO) =
-  Fetch $ join $ do
+  Fetch $ do
     bothFetch <- bothFetchIO
-    return $ case bothFetch of
+    case bothFetch of
       Nothing                -> return Nothing
       Just (!first, !second) -> do
         writeIORef cacheRef second
-        return (Just first)
+        return $ Just first
 
 {-# INLINABLE bothFetchingFirst #-}
 bothFetchingFirst :: IORef b -> Fetch a -> Fetch (a, b)
 bothFetchingFirst cacheRef (Fetch firstFetchIO) =
-  Fetch $ join $ do
-     firstFetch <- firstFetchIO
-     return $ case firstFetch of
-       Nothing            -> return Nothing
-       Just !firstFetched -> do
-         secondCached <- readIORef cacheRef
-         return (Just (firstFetched, secondCached))
+  Fetch $ do
+    firstFetch <- firstFetchIO
+    case firstFetch of
+      Nothing            -> return Nothing
+      Just !firstFetched -> do
+        secondCached <- readIORef cacheRef
+        return $ Just (firstFetched, secondCached)
 
 {-# INLINABLE rightHandlingLeft #-}
 rightHandlingLeft :: (left -> IO ()) -> Fetch (Either left right) -> Fetch right
 rightHandlingLeft handle (Fetch eitherFetchIO) =
-  Fetch $ join $ do
+  Fetch $ do
     eitherFetch <- eitherFetchIO
-    return $ case eitherFetch of
-      Nothing      -> return Nothing
-      Just eitherJ -> case eitherJ of
-        Right !rightInput -> return (Just rightInput)
+    case eitherFetch of
+      Nothing    -> return Nothing
+      Just input -> case input of
+        Right !rightInput -> return $ Just rightInput
         Left  !leftInput  -> handle leftInput $> Nothing
 
 {-# INLINABLE rightCachingLeft #-}
@@ -166,20 +164,20 @@ rightCachingLeft cacheRef =
 {-# INLINABLE eitherFetchingRight #-}
 eitherFetchingRight :: IORef (Maybe left) -> Fetch right -> Fetch (Either left right)
 eitherFetchingRight cacheRef (Fetch rightFetchIO) =
-  Fetch $ join $ do
+  Fetch $ do
     rightFetch <- rightFetchIO
-    return $ case rightFetch of
+    case rightFetch of
       Nothing -> return Nothing
       Just r  -> atomicModifyIORef' cacheRef $ \ case
-        Nothing -> (Nothing, Just (Right r))
-        Just l  -> (Nothing, Just (Left  l))
+        Nothing -> (Nothing, Just $ Right r)
+        Just l  -> (Nothing, Just $ Left  l)
 
 {-# INLINABLE signaling #-}
 signaling :: IO () -> IO () -> Fetch a -> Fetch a
-signaling signalEnd signalElement (Fetch aFetchIO) =
-  Fetch $ join $ do
-    aFetch <- aFetchIO
-    return $ case aFetch of
+signaling signalEnd signalElement (Fetch fetchIO) =
+  Fetch $ do
+    fetch <- fetchIO
+    case fetch of
       Nothing      -> signalEnd $> Nothing
       Just element -> signalElement >> return (Just element)
 
