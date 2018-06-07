8f8ef0b3a4461aeac6d9e9b8ab30803161bdc45b
Mr-Patty <opavlyuchenkov@gmail.com>
2018-06-07 17:00:00 +0300

    Resource check

--- tests/Main.hs
+++ tests/Main.hs
@@ -11,10 +11,12 @@ import qualified Potoki.Core.IO as C
 import qualified Potoki.Core.Consume as D
 import qualified Potoki.Core.Transform as A
 import qualified Potoki.Core.Produce as E
+import qualified Potoki.Core.Fetch as Fe
 import qualified Data.Attoparsec.ByteString.Char8 as B
 import qualified Data.ByteString as F
 import qualified Data.Vector as G
 import qualified System.Random as H
+import qualified Acquire.Acquire as Ac
 import Potoki
 import Transform
 
@@ -32,4 +34,53 @@ main =
     potoki
     ,
     transform
+    ,
+    testCase "sync resource checker" $ do
+      resourceVar <- newIORef Initial
+      let produce = syncChechResource resourceVar
+      C.produceAndConsume produce D.sum
+      fin <- readIORef resourceVar
+      assertEqual "" Released fin
+    ,
+    testCase "async resource checker" $ do
+      resourceVar <- newTVarIO Initial
+      let produce = asyncChechResource resourceVar
+      potokiThreadId <- forkIO $ do
+          C.produceAndConsume produce D.sum
+          return ()
+      atomically $ do
+        resource <- readTVar resourceVar
+        guard $ resource == Acquired
+      killThread potokiThreadId
+      atomically $ do
+        resource <- readTVar resourceVar
+        guard $ resource == Released
   ]
+
+data Resource
+  = Initial
+  | Acquired
+  | Released
+  deriving (Show, Eq)
+
+asyncChechResource :: TVar Resource -> E.Produce Int
+asyncChechResource resourceVar = E.Produce . Ac.Acquire $ do
+  atomically $ writeTVar resourceVar Acquired
+  stVar <- newIORef 0
+  let fetch = do
+        n <- readIORef stVar
+        writeIORef stVar $! n + 1
+        return (Just n)
+  return (Fe.Fetch fetch, atomically $ writeTVar resourceVar Released)
+
+syncChechResource :: IORef Resource -> E.Produce Int
+syncChechResource resourceVar = E.Produce . Ac.Acquire $ do
+  writeIORef resourceVar Acquired
+  stVar <- newIORef 0
+  let fetch = do
+        n <- readIORef stVar
+        if n >= 1000 then return (Nothing)
+        else do
+          writeIORef stVar $! n + 1
+          return (Just n)
+  return (Fe.Fetch fetch, writeIORef resourceVar Released)
