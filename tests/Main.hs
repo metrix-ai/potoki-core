9f0866d7c6881686faa208f5aa8f93a63e424e80
Mr-Patty <opavlyuchenkov@gmail.com>
2018-06-09 13:20:04 +0300

    Monad tests for produce

--- tests/Main.hs
+++ tests/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Prelude hiding (first, second)
 import Control.Arrow
 import Test.QuickCheck.Instances
+import Test.QuickCheck.Monadic as M
 import Test.Tasty
 import Test.Tasty.Runners
 import Test.Tasty.HUnit
@@ -35,16 +36,18 @@ main =
     ,
     transform
     ,
+    resourceChecker
+    ,
     testCase "sync resource checker" $ do
       resourceVar <- newIORef Initial
-      let produce = syncChechResource resourceVar
+      let produce = syncCheckResource resourceVar
       C.produceAndConsume produce D.sum
       fin <- readIORef resourceVar
       assertEqual "" Released fin
     ,
     testCase "async resource checker" $ do
       resourceVar <- newTVarIO Initial
-      let produce = asyncChechResource resourceVar
+      let produce = asyncCheckResource resourceVar
       potokiThreadId <- forkIO $ do
           C.produceAndConsume produce D.sum
           return ()
@@ -57,14 +60,64 @@ main =
         guard $ resource == Released
   ]
 
+resourceChecker :: TestTree
+resourceChecker =
+  testGroup "produce1 >>= produce2" $
+  [
+    testCase "Resource produce1 is released 1 time" $ do
+      resourceVar1 <- newIORef Initial
+      resourceVar2 <- newIORef Initial
+      let prod1 = checkProduce resourceVar1 100
+          prod2 = checkProduce resourceVar2
+      res <- C.produceAndConsume (prod1 >>= prod2) D.sum
+      print res
+      fin <- readIORef resourceVar1
+      assertEqual "" Released fin
+    ,
+    testCase "Fetch1 >>= Fetch2" $ do
+      stVar1 <- newIORef [1]
+      stVar2 <- newIORef [2,3,4]
+      let fetch = Fe.list stVar1 >> Fe.list stVar2
+      let prod = E.Produce $ pure fetch
+      res <- C.produceAndConsume prod D.list
+      assertEqual "" [2,3,4] res
+    ,
+    testProperty "Bind for produce" $ \ (list :: [Int]) ->
+    let check = list >>= (enumFromTo 0)
+        prod1 = E.list list
+        prod2 = \x -> E.list $ enumFromTo 0 x
+    in monadicIO $ do
+      res <- run $ C.produceAndConsume (prod1 >>= prod2) D.list
+      M.assert (check == res)
+  ]
+
 data Resource
   = Initial
   | Acquired
   | Released
+  | ReleasedNonAcquired
   deriving (Show, Eq)
 
-asyncChechResource :: TVar Resource -> E.Produce Int
-asyncChechResource resourceVar = E.Produce . Ac.Acquire $ do
+checkProduce :: IORef Resource -> Int -> E.Produce Int
+checkProduce resourceVar k = E.Produce . Ac.Acquire $ do
+  writeIORef resourceVar Acquired
+  stVar <- newIORef 0
+  let fetch = do
+        n <- readIORef stVar
+        if n >= k then return (Nothing)
+        else do
+          writeIORef stVar $! n + 1
+          print n
+          return (Just n)
+  return $ (,) (Fe.Fetch fetch) $ do
+      res <- readIORef resourceVar
+      case res of
+        Acquired -> writeIORef resourceVar Released
+        _ -> writeIORef resourceVar ReleasedNonAcquired
+
+
+asyncCheckResource :: TVar Resource -> E.Produce Int
+asyncCheckResource resourceVar = E.Produce . Ac.Acquire $ do
   atomically $ writeTVar resourceVar Acquired
   stVar <- newIORef 0
   let fetch = do
@@ -73,8 +126,8 @@ asyncChechResource resourceVar = E.Produce . Ac.Acquire $ do
         return (Just n)
   return (Fe.Fetch fetch, atomically $ writeTVar resourceVar Released)
 
-syncChechResource :: IORef Resource -> E.Produce Int
-syncChechResource resourceVar = E.Produce . Ac.Acquire $ do
+syncCheckResource :: IORef Resource -> E.Produce Int
+syncCheckResource resourceVar = E.Produce . Ac.Acquire $ do
   writeIORef resourceVar Acquired
   stVar <- newIORef 0
   let fetch = do
@@ -83,4 +136,8 @@ syncChechResource resourceVar = E.Produce . Ac.Acquire $ do
         else do
           writeIORef stVar $! n + 1
           return (Just n)
-  return (Fe.Fetch fetch, writeIORef resourceVar Released)
+  return $ (,) (Fe.Fetch fetch) $ do
+      res <- readIORef resourceVar
+      case res of
+        Acquired -> writeIORef resourceVar Released
+        _ -> writeIORef resourceVar ReleasedNonAcquired
