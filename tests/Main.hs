a26dbe2fc63c5f58d4a470baadc15145d6ebb38f
Shevchenko Oleg <Opavlyuchenkov@gmail.com>
2018-08-28 15:29:37 +0300

    Add testing

---
+++ tests/Main.hs
@@ -0,0 +1,32 @@
+module Main where
+
+import Prelude hiding (first, second)
+import Control.Arrow
+import qualified Control.Foldl as Foldl
+import Test.QuickCheck.Instances
+import Test.QuickCheck.Monadic as M
+import Test.Tasty
+import Test.Tasty.Runners
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+import qualified Potoki.Core.IO as IO
+import qualified Potoki.Core.Reduce as Reduce
+import qualified Potoki.Core.Produce as Produce
+
+
+main =
+  defaultMain $
+  testGroup "All tests" $
+  [
+    testProperty "list to list" $ \ (list :: [Int]) ->
+    list === unsafePerformIO (IO.produceAndReduce (Produce.list list) Reduce.list)
+    ,
+    testProperty "list with FoldM" $ \ (list :: [Int]) ->
+    let fold = Foldl.generalize $ Foldl.list
+    in list === unsafePerformIO (IO.produceAndReduce (Produce.list list) (Reduce.foldM fold))
+    ,
+    testProperty "" $ \ (list :: [Int]) ->
+    let fold = Foldl.generalize $ Foldl.list
+    in unsafePerformIO (IO.produceAndReduce (Produce.list list) Reduce.list)
+      === unsafePerformIO (IO.produceAndReduce (Produce.list list) (Reduce.foldM fold))
+  ]
