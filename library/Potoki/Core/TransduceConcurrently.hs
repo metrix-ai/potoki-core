3784e2dcd6a763f8da694dce9e97a12e248a716e
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-09-28 09:54:43 +0300

    Add instances for TransduceConcurrently

--- library/Potoki/Core/TransduceConcurrently.hs
+++ library/Potoki/Core/TransduceConcurrently.hs
@@ -3,5 +3,14 @@ where
 
 import Potoki.Core.Prelude
 import Potoki.Core.Types
-import qualified Potoki.Core.Send as A
+import Potoki.Core.Transduce ()
+import qualified Potoki.Core.Send as Send
 
+
+deriving instance Profunctor TransduceConcurrently
+deriving instance Choice TransduceConcurrently
+deriving instance Strong TransduceConcurrently
+deriving instance Semigroupoid TransduceConcurrently
+deriving instance Category TransduceConcurrently
+deriving instance Arrow TransduceConcurrently
+deriving instance ArrowChoice TransduceConcurrently
